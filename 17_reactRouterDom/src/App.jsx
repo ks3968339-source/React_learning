45a4d1221fe3e89806e7b40660862d02a385be20
kushal sharma <ks3968339@gmail.com>
2026-07-05 12:51:56 +0530

    learned routing basics

---
+++ 17_reactRouterDom/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ 17_reactRouterDom/src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="nav">
+      <h3>Kushals</h3>
+      <div>
+        
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
